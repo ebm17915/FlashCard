0aa1a362933d5bb22ae3706bedbbffe70aef3eea
ebm17915 <earlfredrick.madjus.236@my.csun.edu>
2020-02-21 21:10:43 -0800

    Initial Commit

---
+++ FlashCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCard
+//
+//  Created by Earl on 2/21/20.
+//  Copyright © 2020 Earl Madjus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
