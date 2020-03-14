668e7f9fb9a95854047f572c55dad2e0a360780b
ebm17915 <earlfredrick.madjus.236@my.csun.edu>
2020-03-13 23:58:16 -0700

    new optional Ui updates

--- FlashCard/ViewController.swift
+++ FlashCard/ViewController.swift
@@ -12,9 +12,50 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var Answer: UILabel!
     @IBOutlet weak var Question: UILabel!
-   
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Cards
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.5
+       
+        //Question
+        Question.clipsToBounds = true
+        Question.layer.cornerRadius = 20.0
+        Question.layer.shadowRadius = 15.0
+        Question.layer.shadowOpacity = 0.5
+        
+        //Answer
+        Answer.clipsToBounds = true
+        Answer.layer.cornerRadius = 20.0
+        Answer.layer.shadowRadius = 15.0
+        Answer.layer.shadowOpacity = 0.5
+        
+        // Button 1
+        btnOptionOne.layer.cornerRadius = 15.0
+        btnOptionOne.layer.borderWidth = 3
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0, green: 1, blue: 0.7774125934, alpha: 1)
+        
+        // Button 2
+        btnOptionTwo.layer.cornerRadius = 15.0
+        btnOptionTwo.layer.borderWidth = 3
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0, green: 1, blue: 0.7774125934, alpha: 1)
+        
+        // Button 3
+    
+        btnOptionThree.layer.cornerRadius
+        = 15.0
+        btnOptionThree.layer.borderWidth = 3
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0, green: 1, blue: 0.7774125934, alpha: 1)
+        
         
         Question.isHidden = false
         Answer.isHidden = true
